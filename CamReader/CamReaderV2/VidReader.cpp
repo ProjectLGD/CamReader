653ea0732127e7dc8c19c7cb961c20f2a2d9f5fd
Arthur Brink <arthur.brink1@gmail.com>
2017-12-06 11:08:31 +0100

    Standalone vidreader

---
+++ CamReader/CamReaderV2/VidReader.cpp
@@ -0,0 +1,132 @@
+#include "stdafx.h"
+#include "VidReader.h"
+
+
+VidReader::VidReader()
+{
+	//Initialise values
+
+	hsvstructBlueMin.H = 0;
+	hsvstructBlueMin.S = 200;
+	hsvstructBlueMin.V = 200;
+
+	hsvstructBlueMax.H = 20;
+	hsvstructBlueMax.S = 245;
+	hsvstructBlueMax.V = 255;
+
+	
+	hsvstructRedMin.H = 110;
+	hsvstructRedMin.S = 175;
+	hsvstructRedMin.V = 175;
+	
+	hsvstructRedMax.H = 125;
+	hsvstructRedMax.S = 230;
+	hsvstructRedMax.V = 255;
+
+
+
+}
+
+VidReader::DOT VidReader::getDot(Mat hsv,HSVRANGE range)
+{
+	Mat gray_image;
+
+	inRange(hsv, Scalar(range.min.H, range.min.S, range.min.V), Scalar(range.max.H, range.max.S, range.max.V), gray_image);
+
+	Mat binaryImage;
+	Mat binaryImageShow;
+	threshold(gray_image, binaryImage, 150, 1, CV_THRESH_BINARY);
+	threshold(gray_image, binaryImageShow, 200, 200, CV_THRESH_BINARY);
+	
+	int erosion_size = 2;
+	Mat element = getStructuringElement(MORPH_RECT,
+		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		Point(erosion_size, erosion_size));
+
+	Mat erosion_dst;
+	/// Apply the erosion operation
+	dilate(binaryImage, erosion_dst, element);
+
+	GaussianBlur(erosion_dst, erosion_dst, Size(5, 5), 0);
+
+	Mat binary16S;
+	Mat binary16SShow;
+	erosion_dst.convertTo(binary16S, CV_16S);
+	
+
+	Mat labeledImage2;
+	vector<Point2d *> firstpixelVec2;
+	vector<Point2d *> posVec2;
+	vector<int> areaVec2;
+	labelBLOBsInfo(binary16S, labeledImage2, firstpixelVec2, posVec2, areaVec2);
+
+	Point colorPosition;
+	if (posVec2.size() != 0)
+		colorPosition = Point(firstpixelVec2[0]->x, firstpixelVec2[0]->y);
+	
+	DOT foundDot;
+	foundDot.color = range.name;
+	foundDot.pt = colorPosition;
+	
+	return foundDot;
+}
+
+vector<VidReader::DOT> VidReader::getPositions()
+{
+	VideoCapture cap(0);
+
+	if (!cap.isOpened())
+		cout << "Cannot open the video cam" << endl;
+
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+
+	Mat frame;
+
+	//Setup search range
+	vector<HSVRANGE> colors;
+	HSVRANGE red;
+	red.name = "RED";
+	red.min = hsvstructRedMin;
+	red.max = hsvstructRedMax;
+
+	HSVRANGE blue;
+	blue.name = "BLUE";
+	blue.min = hsvstructBlueMin;
+	blue.max = hsvstructBlueMax;
+
+	colors.push_back(red);
+	colors.push_back(blue);
+
+
+	bool bSuccess = cap.read(frame);
+
+
+	if (!bSuccess)
+		cout << "Cannot read a frame from video stream" << endl;		
+
+
+	Mat hsv;
+
+	cvtColor(frame, hsv, COLOR_RGB2HSV);
+	imshow("test", frame);
+	std::cout << "start" << std::endl;
+	waitKey(1);
+
+	vector<DOT> positions;
+
+
+
+	for (HSVRANGE range : colors)
+		positions.push_back(getDot(hsv, range));
+
+
+	return positions;
+
+
+}
+
+VidReader::~VidReader()
+{
+}
---
+++ CamReader/CamReaderV2/dot.h
@@ -0,0 +1,7 @@
+#pragma once
+enum DOT_TYPE{RED,BLUE};
+
+struct DOT {
+	DOT_TYPE color;
+	Point pt;
+};
